916ef571e654259044bb58c285f8cc0f379b415c
CBGhaha <875675253@qq.com>
2018-07-17 15:18:22 +0800

    Lazy Loading Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Page1 from 'views/page1'
-import Page2 from 'views/page2'
 
 Vue.use(Router)
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'page1',
-      component: Page1
-    },
-    {
-      path: '/page2',
-      name: 'page2',
-      component: Page2
+const routerMaps=[
+  {
+    path: '/',
+    name: 'page1',
+    component: 'Page1'
+  },
+  {
+    path: '/page2',
+    name: 'page2',
+    component: 'Page2'
+  }
+];
+function ansysLoad(item){
+  return (resolve)=>{
+      import (/* webpackChunkName: item.name */(`../views/${item.name}/index.vue`)).then(
+        (module)=>{
+          resolve(module)
+        }
+      )
+  }
+}
+
+const routers=routerMaps.map((item)=>{
+    let component;
+    if(typeof item.component==='object'){
+      component=item.component;
+    }else if(typeof item.component==='string'){
+      component=ansysLoad(item);
     }
-  ]
+    return Object.assign(item,{component});
+
+})
+export default new Router({
+  routes:routers
 })
